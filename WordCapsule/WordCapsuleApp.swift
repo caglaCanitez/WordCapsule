f13fb1361fba73a86937ffc4de7e2cb550875e6a
Cagla Canitez <d-ccanitez@ziraatteknoloji.com>
2023-11-08 23:42:43 +0300

    Created the project

---
+++ WordCapsule/WordCapsuleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordCapsuleApp.swift
+//  WordCapsule
+//
+//  Created by Cagla CANITEZ on 8.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct WordCapsuleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
